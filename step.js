6ceeb2649666a21b34e62327bb072849adab6531
Zé <zepspaiva@gmail.com>
2016-12-02 19:24:49 -0300

    .

--- step.js
+++ step.js
@@ -54,42 +54,13 @@ Step.prototype.recognize = function(html) {
 
 };
 
-Step.prototype._levenshtein = function(str1, str2) {
-
-	if (str1 == null && str2 == null) return 0;
-	if (str1 == null) return String(str2).length;
-	if (str2 == null) return String(str1).length;
-
-	str1 = String(str1); str2 = String(str2);
-
-	var current = [], prev, value;
-
-	for (var i = 0; i <= str2.length; i++)
-		for (var j = 0; j <= str1.length; j++) {
-			if (i && j)
-				if (str1.charAt(j - 1) === str2.charAt(i - 1))
-	  				value = prev;
-				else
-	  				value = Math.min(current[j], current[j - 1], prev) + 1;
-			else
-				value = i + j;
-
-			prev = current[j];
-			current[j] = value;
-		}
-
-	return current.pop();
-
-};
-
 Step.prototype.injectCode = function(html, taskexecuuid) {
 
 	var self = this;
 
 	html += ['<script type="text/javascript" src="/', self.prefix, 'static/jquery-latest.min.js"></script>'].join('');
 	html += ['<script type="text/javascript" src="/', self.prefix, 'static/spin.min.js"></script>'].join('');
-
-	// html += ['<script type="text/javascript"> $("body").spin("modal","', self.name, '"); </script>'].join('');
+	html += ['<script type="text/javascript" src="/', self.prefix, 'static/webrobot.js"></script>'].join('');
 
 	var timeoutbegin = 'setTimeout(function() {';
 	var timeoutend = '}, 10);';
@@ -125,31 +96,8 @@ Step.prototype.injectCode = function(html, taskexecuuid) {
 			} else if (field.value) {
 				
 				if (field.type == 'select') {
-
-					var options = [];
-					$(selector + ' option').each(function(a, b) {
-						options.push({ text: b.text, value: b.value });
-					});
-
-					var validoption = null;
-					var validoptiondist = 100000;
-
-					for (var i = 0; i < options.length; i++) {
-						var option = options[i];
-						var dist = _levenshtein(option.text, field.value);
-
-						console.log('calc dist', dist, validoptiondist);
-
-						if (dist < validoptiondist) {
-							validoption = option;
-							validoptiondist = dist;
-						}
-					}
-
-					console.log('validoption', validoption);
 					
-					if (validoption)
-						html += ['$(\'',selector, '\').val(\'', validoption.value,'\')', field.trigger ? '.trigger(\'' + field.trigger + '\');' : ';'].join('');
+					html += ['selectclosest(\'',selector, '\', \'', field.value,'\')', field.trigger ? '.trigger(\'' + field.trigger + '\');' : ';'].join('');
 
 				} else {
 					
